15289b804b1547b4dbf4e993b2d6e7e308df88ab
Petya Naydenova Ferreira <pn@abtion.com>
2024-09-19 13:23:33 +0200

    fix errors found by eslint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,6 +31,8 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
+    console.log(error);
+
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -56,6 +58,8 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (error) {
+    console.log(error);
+
     return {
       message: "Database Error: Failed to Update Invoice.",
     };
@@ -75,6 +79,8 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
+    console.log(error);
+
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
